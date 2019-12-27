18a7c166b4108fdd98daedfa1e03de748f96b33d
Travis DePrato <travisdeprato@gmail.com>
2019-12-26 19:59:13 -0800

    Files generated by PkgTemplates

---
+++ src/WeakValueDict.jl
@@ -0,0 +1,5 @@
+module WeakValueDict
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using WeakValueDict
+using Test
+
+@testset "WeakValueDict.jl" begin
+    # Write your own tests here.
+end
